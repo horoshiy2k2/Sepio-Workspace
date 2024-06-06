5bd8919885578115f26c9957e8a2e1a2ef85c6e0
OleksandrBaranenko <oleksandr.baranenko@incloudlogic.com>
2024-06-06 13:07:39 +0300

    hashMiddleware

--- Sepio-App/backend/src/middlewares/hashMiddleware.js
+++ Sepio-App/backend/src/middlewares/hashMiddleware.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const hashPassword = async (req, res, next) => {
   try {
     if (req.body.password) {
-      const saltRounds = 10;
+      const saltRounds = parseInt(process.env.SALT_ROUNDS, 10);
       const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
       req.body.password = hashedPassword;
     }
